a8346864a76b5a419999f3da5cbbab30e871d112
charr1309 <ceh710@aol.com>
2021-04-18 08:02:41 -0400

    added images, created fetch statement

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Hello World!\n==========\n");
 
 // PROJECT Section
 console.log("PROJECT:\n==========\n");
+
+fetch(https:"//api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={d335ed2d61e33f3cdce01f7e461e3429}")
