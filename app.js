f1bcbdf648fb20fd75b101636df155e619dda4ef
charr1309 <ceh710@aol.com>
2021-04-18 10:17:18 -0400

    added .then statements

--- app.js
+++ app.js
@@ -2,5 +2,19 @@ console.log("Hello World!\n==========\n");
 
 // PROJECT Section
 console.log("PROJECT:\n==========\n");
+const city = document.getElementById("city");
+const state = document.getElementById("state");
+const submit = document.getElementById("submit").addEventListener("click", function(){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={d335ed2d61e33f3cdce01f7e461e3429}")
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
 
-fetch(https:"//api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={d335ed2d61e33f3cdce01f7e461e3429}")
+    })
+    .catch(e => {
+        console.log("Error retrieving data", e)
+    })
+})
+
+
